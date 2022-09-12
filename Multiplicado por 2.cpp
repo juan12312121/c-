c70eec52dbbc16956caa09dcbe48bde5cab980d9
juan12312121 <97489315+juan12312121@users.noreply.github.com>
2022-09-11 18:46:10 -0600

    Add files via upload

---
+++ Multiplicado por 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int x;
+	
+	cout<<"ingrese un numero";
+	cin>>x;
+	
+	cout<<"el doble del numero es:" <<x * 2;
+	
+	
+	
+	
+	return 0;
+	
+	
+	
+	
+}
